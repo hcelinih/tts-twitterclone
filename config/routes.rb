80f836b236c36db015c8cbe5ec30b2b328289b7c
Hope Henderson <hcelinih@gmail.com>
2017-03-15 21:03:02 -0500

    Add Tweets Controller and set CRUD routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+resources :tweets
+
   devise_for :users
+
+  root 'tweet#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
