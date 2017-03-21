5b666847778de0176e926714f74e9cdc8e1ce46b
Hope Henderson <hcelinih@gmail.com>
2017-03-20 19:28:16 -0500

    Add ability to create and read tweets
    * Add new and create methods / views
    * Add show view

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,13 +1,40 @@
 class TweetsController < ApplicationController
+  before_action :authenticate_user!
+
   def index
   end
 
   def show
+    @tweet = Tweet.find(params[:id])
   end
 
   def edit
   end
 
   def new
+    @tweet = Tweet.new
+  end
+
+  def create
+    @tweet = Tweet.new(tweet_params)
+
+    respond_to do |format|
+      if @tweet.save
+          format.html { redirect_to @tweet, notice: 'You just tweeted!' }
+      else
+        format.html { render :new }
+      end
+    end
+  end
+
+
+  private
+
+  def tweet_params
+    params.require(:tweet).permit(:message, :user_id)
+
   end
 end
+
+
+
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ resources :tweets
 
   devise_for :users
 
-  root 'tweet#index'
+  root 'tweets#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
