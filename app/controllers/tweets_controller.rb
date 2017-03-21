30c609fb49f5d3cb59ffc93e19893d7b7b5c7013
Hope Henderson <hcelinih@gmail.com>
2017-03-20 20:32:29 -0500

    Updated all tweets in order to edit

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,14 +1,25 @@
 class TweetsController < ApplicationController
   before_action :authenticate_user!
+  before_action :set_tweet, only: [:show, :edit, :update]
 
   def index
+    @tweets = Tweet.all
   end
 
   def show
-    @tweet = Tweet.find(params[:id])
   end
 
   def edit
+  end
+
+  def update
+    respond_to do |format|
+      if @tweet.update(tweet_params)
+        format.html {redirect_to @tweet, notice: 'Your tweet has been updated.'}
+      else
+        format.html {render :edit}
+      end
+    end
   end
 
   def new
@@ -32,6 +43,10 @@ class TweetsController < ApplicationController
 
   def tweet_params
     params.require(:tweet).permit(:message, :user_id)
+  end
+
+  def set_tweet
+    @tweet = Tweet.find(params[:id])
 
   end
 end
