5f406135506aa0c064d9aeaac083749801e17914
Hope Henderson <hcelinih@gmail.com>
2017-03-22 18:26:44 -0500

    Add user relationship table and set relationship model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170316015456) do
+ActiveRecord::Schema.define(version: 20170322231753) do
+
+  create_table "relationships", force: :cascade do |t|
+    t.integer  "user_id"
+    t.integer  "friend_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "tweets", force: :cascade do |t|
     t.string   "message"
