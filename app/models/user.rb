62d48825820fa75c216df36287375ccad82ba3c9
Hope Henderson <hcelinih@gmail.com>
2017-03-22 20:32:49 -0500

    Added likes to tweets

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,4 +14,8 @@ class User < ApplicationRecord
   has_many :likes
 
   validates :username, presence: true, uniqueness: true
+
+  def likes?(tweet)
+    tweet.likes.where(user_id: id).any?
+  end
 end
